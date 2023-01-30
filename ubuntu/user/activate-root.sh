a70ff49dd0942bad4cfada064394d8ceb1a97f5e
Milad Khodaverdi <khodaverdi.milad23@gmail.com>
2023-01-31 02:51:07 +0330

    Update activate-root.sh

--- ubuntu/user/activate-root.sh
+++ ubuntu/user/activate-root.sh
@@ -9,8 +9,25 @@ sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh
 # Restart ssh service to apply changes
 sudo systemctl restart ssh
 
-# Change root password
+while :
+do
+  # Take new_password from user
+  echo "Enter new password for the root user:"
+  read -s new_password
+  echo
+  echo "Confirm new password for the root user:"
+  read -s new_password_confirm
+  echo
+
+  # Check if passwords match
+  if [ "$new_password" = "$new_password_confirm" ]; then
+    break
+  else
+    echo "Passwords do not match. Please try again."
+  fi
+done
+
 echo "Changing root password"
-sudo passwd root
+sudo echo "root:$new_password" | sudo chpasswd
 
 echo "Root user has been activated and password has been set. Please log in using root user and the new password."
